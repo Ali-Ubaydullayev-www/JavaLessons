7c0c5872af8766e9b3543c481ae513cf8f428204
Ali <mukhammadali.ubaydullayev.05@inbox.ru>
2021-10-21 12:51:40 +0500

    I enter +-*/ of JS

---
+++ lesson-1/index.js
@@ -0,0 +1,27 @@
+let a = '15';
+a = parseInt(a);
+// console.log(typeof(a));
+// parseInt - str изменяет на number
+
+
+
+let b = '15';
+b = parseFloat(b);
+// console.log(typeof(b));
+// parseFloat - изменяет на (число с плавающей точкой)Float
+
+
+
+let c = '1551541.4865445';
+c = parseFloat(c);
+c.toFixed(2);
+// console.log(c);
+// console.log(typeof(c));
+
+
+
+
+
+
+
+
