66a6fb537c4f0500ed1fead775f5d1fca244676a
Ali <65356558+Ali-Ubaydullayev-www@users.noreply.github.com>
2021-11-05 15:06:03 +0500

    Add files via upload

---
+++ Js-8/script.js
@@ -0,0 +1,59 @@
+/* *********** -- Example-1 -- ************** */
+
+fruits = ['apple', 'ananas', 'orange'];
+fruits = fruits.toString()
+a = 0;
+b = 0;
+for (let i = 0; i < fruits.length; i++) {
+    (fruits[i] == 'a' ||  fruits[i] == 'A') && a++;
+}
+console.log('b = ', b);
+console.log('a = ', a);
+
+/* *********** -- Example-2 -- ************** */
+
+// number = [10, 5, 11, 'apple', 2, 3, 4, 5, 6]
+// result = 0;
+// for (i = 0; i < number.length; i++) {
+//     if (typeof number[i] === 'number') {
+//         result = result + number[i]
+//     }
+// }
+// console.log('oll result = ', result);
+
+/* *********** -- Example-3 -- ************** */
+
+
+
+/* *********** -- Example-4 -- ************** */
+
+// even = 0;
+// odd = 0; 
+// number = [10, 5, 11, 2, 3, 4, 5, 6]
+// for (i = 0; i < number.length; i += 2){
+//     even += number[i]
+// }
+// for (i = 1; i < number.length; i += 2){
+//     odd += number[i]
+// }
+// console.log('Even = ', even)
+// console.log('Odd', odd);
+
+/* *********** -- Example-5 -- ************** */
+
+// text = ['5', '4', '3', '2', '1']
+// texti = text.length
+// let result = [];
+// for (texti; texti > -1; texti--) {
+//     result.push(text[texti]) 
+// }
+// result.shift();
+// console.log(result);
+
+/* *********** -- Example-6 -- ************** */
+
+
+
+
+
+
