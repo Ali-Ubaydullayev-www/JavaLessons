9d94338f2a49cb90a050cc7dcda9298210f4b8e8
Ali <mukhammadali.ubaydullayev.05@inbox.ru>
2021-11-12 15:05:33 +0500

    start

---
+++ lesson-5/script.js
@@ -0,0 +1,19 @@
+
+// let a = [1,1,3,4,];
+
+    
+// a.sort(function(a, b){return a - b});
+// for(i=0;i<a.length;i++){
+//     let s = a.length-1;
+//     let d = a.length-2;
+//    let a1 =  a.splice(s,1) - a.splice(d,1);
+
+//    if(a1!==0){
+//        a.push(a1)
+// a.sort(function(a, b){return a - b});
+
+//    }
+// if(a.length===1){
+// console.log(a)
+// }
+// }
